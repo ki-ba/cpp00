709713351255a82c849460bc5386c5a22b9e0a18
Killian Barru <shaper.vaquero-03@icloud.com>
2025-10-27 12:57:43 +0100

    improve testing script readability

--- ex02/runtest.sh
+++ ex02/runtest.sh
@@ -8,6 +8,12 @@ cat 19920104_091532.log | awk '{ print $2 }' > $logfile
 ./job | awk '{print $2}' > $mylog
 
 diff $logfile $mylog
+ok=`diff $logfile $mylog | wc -l`
+if [ $ok -eq 0 ]; then
+	echo "ok, no diff in log"
+else
+	echo "ERROR : $ok differences found in log"
+fi
 
 rm $logfile $mylog
 
